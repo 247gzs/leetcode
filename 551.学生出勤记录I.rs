a2042d12043811baded2a49bd2bbf0960cba1e9c
wuxiushu <wuxiushu@mokuantouzi.com>
2021-09-18 13:30:11 +0800

    fix

--- 551.学生出勤记录I.rs
+++
@@ -1,8 +0,0 @@
-impl Solution {
-    pub fn check_record(s: String) -> bool {
-        if s.chars().filter(|&c| c == 'A').count() < 2 && !s.contains("LLL") {
-            return true;
-        }
-        false
-    }
-}
