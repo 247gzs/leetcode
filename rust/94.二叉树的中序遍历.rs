63eb1be9c4ff6a1b16b8565ab68a22f812af698c
wuxiushu <wuxiushu@mokuantouzi.com>
2021-09-23 18:38:27 +0800

    中序遍历（LDR）

---
+++ rust/94.二叉树的中序遍历.rs
@@ -0,0 +1,35 @@
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+//
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
+use std::rc::Rc;
+use std::cell::RefCell;
+impl Solution {
+    pub fn inorder_traversal(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+        fn in_order(node: Option<Rc<RefCell<TreeNode>>>, vec: &mut Vec<i32>) {
+            if let Some(mut node) = node {
+                in_order(node.borrow_mut().left.take(), vec);
+                vec.push(node.borrow().val);
+                in_order(node.borrow_mut().right.take(), vec);
+            }
+        }
+
+        let mut ret = Vec::new();
+        in_order(root, &mut ret);
+        ret
+    }
+}
