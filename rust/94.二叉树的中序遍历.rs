99cfdc370741f07c609ebb8d85a8afc9df7f85da
wuxiushu <wuxiushu@mokuantouzi.com>
2021-10-09 17:36:57 +0800

    二叉树的中序遍历

--- rust/94.二叉树的中序遍历.rs
+++ rust/94.二叉树的中序遍历.rs
@@ -20,16 +20,16 @@ use std::rc::Rc;
 use std::cell::RefCell;
 impl Solution {
     pub fn inorder_traversal(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
-        fn in_order(node: Option<Rc<RefCell<TreeNode>>>, vec: &mut Vec<i32>) {
-            if let Some(mut node) = node {
-                in_order(node.borrow_mut().left.take(), vec);
-                vec.push(node.borrow().val);
-                in_order(node.borrow_mut().right.take(), vec);
+        fn dfs(node: &Option<Rc<RefCell<TreeNode>>>, result: &mut Vec<i32>) {
+            if let Some(x) = node {
+                dfs(&x.borrow().left, result);
+                result.push(x.borrow().val);
+                dfs(&x.borrow().right, result);
             }
         }
 
-        let mut ret = Vec::new();
-        in_order(root, &mut ret);
-        ret
+        let mut ans = vec![];
+        dfs(&root, &mut ans);
+        ans
     }
 }
